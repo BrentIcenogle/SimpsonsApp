560b509fdf314f1ed0f4c4198de7423d2db0ea7e
BrentIcenogle <brent.icenogle@outlook.com>
2014-04-26 15:41:38 -0400

    SimpsonsApp
    Info: Angular app provides detailed information on Simpsons characters
    Author: Brent Icenogle
    Updated: 4/26/2014

---
+++ javascript/app.js
@@ -0,0 +1,25 @@
+﻿/*
+Javascript for Routing The Simpsons Character Angular Application
+Author: Brent Icenogle
+Updated: April 25, 2014
+*/
+
+//Module Definition
+var simpsonsApp = angular.module('simpsonsApp', ['ngRoute', 'SimpsonsControllers', 'ngAnimate']);
+
+//Routing Definition
+simpsonsApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/characters', {
+            templateUrl: 'html/characters.html',
+            controller: 'CharactersController'
+        }).
+        when('/about', {
+            templateUrl: 'html/about.html',
+            controller: 'CharactersController'
+        }).
+        otherwise({
+            redirectTo: '/characters'
+        });
+  }]);
